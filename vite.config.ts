6bfacc4d8e46629a072a586a4d4f2ebfafbc193a
kfd <kfd_personal@163.com>
2025-08-29 10:40:53 +0800

    fix: cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path, { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -22,6 +23,44 @@ export default defineConfig({
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: "generateSW",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000,
+        globPatterns: ['**/*.{css,ico,png,svg,wasm,js}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/cdn\.kongfandong\.cn\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'cdn-cache',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 7 // <== 7 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: "Web Office",
+        short_name: "Web Office",
+        description: "在线Web版Office, 基于Only office",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "64.png",
+            sizes: "192x192",
+            type: "image/png",
+          }
+        ],
+      },
+    })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
